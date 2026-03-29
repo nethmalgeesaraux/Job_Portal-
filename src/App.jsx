2975ac3d576f948e23620070b9540c7ed971d8b8
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-03-29 20:50:47 +0530

    routes pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Applyjob from './pages/Applyjob'
+import Applications from './pages/Applications'
 
 const App = () => {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/applyjob/:id' element={<Applyjob/>}/>
+      <Route path='/applications' element={<Applications/>}/>
+     </Routes>
     </>
   )
 }
